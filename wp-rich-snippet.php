dce551b627be62411ef11b4c2335fd07b9527403
Dale Bengston <dale.bengston@gmail.com>
2018-02-27 13:18:10 -0500

    Added comment block and boilerplate

--- wp-rich-snippet.php
+++ wp-rich-snippet.php
@@ -1,4 +1,43 @@
 <?php
 /*
 Plugin Name: WP Rich Snippet
+Plugin URI: https://www.streamline-studio.com/plugins/
+Description: Add local SEO to each page in JSON-LD format, preferred by Google.
+Version: 1.0.0
+Author: Dale Bengston, Streamline Studio, LLC
+Author URI: https://www.streamline-studio.com
+
+	Copyright 2018 Dale Bengston
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if( !defined( 'WPRICHSNIPPET_VER' ) )
+	define( 'WPRICHSNIPPET_VER', '1.0.0' );
+
+
+https://github.com/claudiosanches/wordpress-plugin-boilerplate.git
+/*
+register_activation_hook( __FILE__, 'pluginprefix_function_to_run' );
+
+
+
+
+register_deactivation_hook( __FILE__, 'pluginprefix_function_to_run' );
+
+
+
+
+register_uninstall_hook(__FILE__, 'pluginprefix_function_to_run');
 */
